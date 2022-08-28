21c43b55cc3c3b214fa10e4d2c5820b04d4f21ff
kavink_r <k.r.kavin007@gmail.com>
2022-08-28 10:45:23 +0530

    Added practice file ecommerce

---
+++ src/Practice/src/ecommerce/myClass.java
@@ -0,0 +1,13 @@
+package ecommerce;
+
+public class myClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Product p1 = new Product();
+		p1.readData();
+		p1.printData();
+		
+	}
+
+}
